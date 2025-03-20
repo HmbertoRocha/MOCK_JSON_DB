0425a90c2f1023936e3de5bc1af06b2047c70445
HmbertoRocha <hrn72thousand@gmail.com>
2025-03-19 18:23:26 -0600

    update

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
 require('dotenv').config();
+const PORT = process.env.PORT;
+
 
 //Setup the view engine 
 app.set('view engine', 'ejs');
 app.set('viewa', './views');
+
+//Middleware
+app.use((req, res, next)=>{
+    const time = new Date();
+
+
+    console.log(time, req.url,req.method);
+
+
+    next();
+});
 
 
 app.listen(PORT, ()=>{
